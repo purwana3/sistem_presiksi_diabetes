a4f410c60c13767a26615e0fef1f1e782729c726
purwana3 <rijkivirmansah@gmail.com>
2025-04-26 18:55:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,4 +39,5 @@ def predict():
         return f"Terjadi kesalahan: {str(e)}", 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
